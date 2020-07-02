7847b16b6812f6a7c2bbd1b238ac6ea1d3dc6178
jaechang <jaechang0721k@gmail.com>
2020-07-02 17:18:54 +0900

    Platformer

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool instance;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
+
+    [SerializeField]
+    public List<GameObject> objectsPool = new List<GameObject>();
+
+    int poolSize = 20;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            objectsPool.Add(Instantiate(prefab,Vector3.zero,Quaternion.identity, this.transform));
+            objectsPool[i].SetActive(false);
+        }
+        //StartCoroutine(CreTest());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator CreTest()
+    {
+        while (true)
+        {
+            GameObject temp;
+            temp = objectsPool[0];
+            temp.SetActive(true);
+            Debug.Log(this.transform.root.name);
+            temp.transform.SetParent(null);
+            objectsPool.RemoveAt(0);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    public void PushOfObjectPool(List<GameObject> PoolName, GameObject PushObject)
+    {
+        PoolName.Add(PushObject);
+        PushObject.transform.SetParent(this.transform);
+    }
+
+    public void PullOfObjectPool(List<GameObject> PoolName)
+    {
+        if (PoolName.Count != 0)
+        {
+            GameObject temp = PoolName[0];
+            temp.transform.SetParent(null);
+            temp.SetActive(true);
+            PoolName.Remove(temp);
+        }
+        else
+        {
+            GameObject temp = Instantiate(prefab, Vector3.zero, Quaternion.identity, this.transform);
+            PoolName.Add(temp);
+            temp.SetActive(false);
+
+            temp = PoolName[0];
+            temp.transform.SetParent(null);
+            temp.SetActive(true);
+            PoolName.Remove(temp);
+        }
+
+    }
+
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class MoveData
 {
     public float moveSpeed;
     public float jumpSpeed;
-
+    public float damage;
+    public int jumpCount;
+    public bool isJump;
+    public bool isDubleJump;
+    public bool isGrounded;
+    
 }
 
 
 public class PlayerMovement : MonoBehaviour
 {
     public MoveData moveData = new MoveData();
-
+    
 
     Rigidbody m_physics;
     Transform m_tr;
@@ -22,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
     public Vector3 m_velocity;
     float horizontal;
     float vertical;
-    
+    public Vector3 target = Vector3.zero;
     void Start()
     {
         m_tr = this.GetComponent<Transform>();
@@ -35,20 +41,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        MoveFunc();
+        m_tr.position = Vector3.MoveTowards(m_tr.position, target, Time.deltaTime * constF);
+
         
-#if UNITY_EDITOR
-
-        horizontal = Input.GetAxis("Horizontal");
-        //vertical = Input.GetAxis("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            m_physics.velocity = m_physics.velocity + Vector3.up * moveData.jumpSpeed;
-            Debug.Log("Input");
-        }
-
-#endif
-        m_physics.velocity = new Vector3(horizontal*moveData.moveSpeed, m_physics.velocity.y);
-
 
     }
 
@@ -63,7 +59,104 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            moveData.isGrounded = true;
+        }
+        moveData.jumpCount = 2;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            moveData.isGrounded = false;
+        }
+    }
+
+    public void MoveFunc()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        //vertical = Input.GetAxis("Vertical");
+        JumpFunc();
+
+        Dash();
+        target = new Vector3(target.x + horizontal * moveData.moveSpeed * Time.deltaTime, m_tr.position.y, m_tr.position.z);
+        
+
+    }
+
+    private void JumpFunc()
+    {
+        if (moveData.jumpCount > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_physics.velocity = m_physics.velocity + Vector3.up * moveData.jumpSpeed;
+                Debug.Log("Input");
+                moveData.jumpCount--;
+            }
+        }
+    }
 
 
+    public float constF;
+    public float deltaTime;
+    public KeyCode beforeKey = KeyCode.None;
+    private void Dash()
+    {
+        deltaTime += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (beforeKey == KeyCode.RightArrow && deltaTime < 1.0f)
+            {
+                deltaTime++;
+                target = new Vector3(m_tr.position.x + 5, target.y, target.z);
+                
+            }
+            else
+            {
+                beforeKey = KeyCode.RightArrow;
+                deltaTime = 0;
+            }
+            
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (beforeKey == KeyCode.LeftArrow && deltaTime < 1.0f)
+            {
+                deltaTime++;
+                target = new Vector3(m_tr.position.x - 5, target.y, target.z);
+            }
+            else
+            {
+                beforeKey = KeyCode.LeftArrow;
+                deltaTime = 0;
+            }
+        }
+    }
+
+    public void PressLeft()
+    {
+        Button btn = GetComponent<Button>();
+        //btn.onClick.AddListener();
+    }
+
+    public void PressRight()
+    {
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("OnTrigger");
+            target = Vector3.zero;
+            this.gameObject.transform.position = Vector3.zero;
+        }
+    }
 
 }
